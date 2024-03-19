6bbb0b3e59e361aaa0d783b74be9ffbf4dd2accd
ronCdeTracker <2fixelectronics777@gmail.com>
2024-03-19 00:03:43 -0500

    super mod

---
+++ models/users-superagent.mjs
@@ -0,0 +1,21 @@
+
+
+
+import { default as request } from 'superagent';
+import util from 'util';
+import url from 'url';
+const URL = rul.URL;
+import DBG from 'debug';
+const debug = DBG('notes:users-superagent');
+const error = DBG('notes:error-superagent');
+
+var authid = 'them';
+var authpass = 'D4ED43C0-8BD6-4FE2-B358-7C0E230D11EF';
+
+function reqURL(path) {
+    const requrl = new URL(process.env.USER_SERVICE_URL);
+    requrl.pathname = path;
+    return requrl.toString();
+}
+
+
