a638cb6f8fea4c180e10aa59607b36c0b26dbe9c
ronCodeTracker <2fixelectronics777@gmail.com>
2024-06-18 21:04:30 -0500

    last section start mod

--- routes/notes.mjs
+++ routes/notes.mjs
@@ -6,9 +6,9 @@ import { default as express } from 'express';
 import { NotesStore as notes } from '../models/notes-store.mjs';
 export const router = express.Router();
 
-import { default as DBG } from 'debug';
-const debug = DBG('notes:debug');
-const dbgerror = DBG('notes:error'); 
+//import { default as DBG } from 'debug';
+//const debug = DBG('notes:debug');
+//const dbgerror = DBG('notes:error'); 
 
 //const capcon = 'capture-console'
 import * as capcon from 'capture-console';
@@ -17,6 +17,15 @@ import { ensureAuthenticated } from './users.mjs';
 
 import { emitNoteTitles } from './index.mjs';
 import { io } from '../app.mjs'; // for ch 9
+
+import {
+    postMessage, destroyMessage, recentMessages, emitter as msgEvents
+} from '../models/messages-sequelize.mjs';// for ch 9 messages later section
+
+import DBG from 'debug';
+const debug = DBG('notes:home');
+const error = DBG('notes:error-home');
+
 
 //let stdout;
 
@@ -125,5 +134,55 @@ router.post('/destroy/confirm', ensureAuthenticated, async (req, res, next) => {
 
 
 export function init() {
+    notes.on('noteupdated', note => {
+        const toemit = {
+            key: note.key, title: note.title, body: note.body
+        };
+        debug(`noteupdated to ${note.key} ${util.inspect(toemit)}`);
+        io.of('/notes').to(note.key).emit('noteupdated', toemit);
+        emitNoteTitles();
+    });
+    notes.on('notedestroyed', key => {
+        debug(`notedestroyed to ${key}`);
+        io.of('/notes').to(key).emit('notedestroyed', key);
+        emitNoteTitles();
+    });
 
+    msgEvents.on('newmessage', newmsg => {
+        debug(`newmessage ${util.inspect(newmsg)} ==> ${newmsg.namespace} ${newmsg.room}`);
+        io.of(newmsg.namespace).to(newmsg.room).emit('newmessage', newmsg);
+    });
+    msgEvents.on('destroymessage', data => {
+        debug(`destroymessage ${util.inspect(data)} ==> ${data.namespace} ${data.room}`);
+        io.of(data.namespace).to(data.room).emit('destroymessage', data);
+    });
+
+
+    io.of('/notes').on('connect', async (socket) => {
+        let notekey = socket.handshake.query.key;
+        debug(`/notes browser connected on ${socket.id} ${util.inspect(socket.handshake.query)}`);
+        if (notekey) {
+            socket.join(notekey);
+
+            socket.on('create-message', async (newmsg, fn) => {
+                try {
+                    debug(`socket createMessage ${util.inspect(newmsg)}`);
+                    await postMessage(
+                        newmsg.from, newmsg.namespace, newmsg.room,
+                        newmsg.message);
+                    fn('ok');
+                } catch (err) {
+                    error(`FAIL to create message ${err.stack}`);
+                }
+            });
+
+            socket.on('delete-message', async (data) => {
+                try {
+                    await destroyMessage(data.id);
+                } catch (err) {
+                    error(`FAIL to delete message ${err.stack}`);
+                }
+            });
+        }
+    });
 }
